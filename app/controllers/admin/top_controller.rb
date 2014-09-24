f828cef0e7934af8ad59fa8fd0bdc66f9a791b01
weblike-guy <toshi.newapps.info@gmail.com>
2014-09-25 03:13:35 +0700

    first commit

---
+++ app/controllers/admin/top_controller.rb
@@ -0,0 +1,5 @@
+class Admin::TopController < ApplicationController
+  def index
+    render ation: 'index'
+  end
+end
---
+++ app/controllers/staff/top_controller.rb
@@ -0,0 +1,6 @@
+class Staff::TopController < ApplicationController
+
+  def index
+   render action: 'index'
+  end
+end
